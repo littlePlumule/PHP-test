dd1591aac595a5b7bcab3e1b1d316efcdbaf59b2
hung <ff789456741@gmail.com>
2021-09-23 15:03:49 +0800

    create handle_add & connect

--- jobs/handle_add.php
+++ jobs/handle_add.php
@@ -1,10 +1,23 @@
 <?php
-
+require_once('./connect.php');
 $title = $_POST['title'];
 $desc = $_POST['description'];
 $salary = $_POST['salary'];
 $link = $_POST['link'];
 
-echo $title." ".$desc." ".$salary." ".$link;
+if(empty($title) || empty($desc) || empty($salary) || empty($link)){
+    die("<h1>請檢查資料</h1>");
+}
+
+
+$sql = "INSERT INTO jobs(title,description,salary,link) VALUES('$title','$desc','$salary','$link')";
+
+$result = $conn->query($sql);
+
+if($result){
+    header('Location: ./admin.php');
+}else{
+    echo "falled： " . $conn->error;
+}
 
 ?>
